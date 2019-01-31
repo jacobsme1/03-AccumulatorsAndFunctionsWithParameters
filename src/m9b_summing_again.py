11cae01eeb12b8a34b6193adc7bb98ccc9e3f91b
Max Jacobs <jacobsme@rose-hulman.edu>
2019-01-31 14:37:49 -0500

    all todo's done.

--- src/m9b_summing_again.py
+++ src/m9b_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Max Jacobs.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,21 @@ def run_test_sum_powers():
     print('--------------------------------------------------')
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
+    #Test1
+    expected1 = 6
+    print('expected:', expected1)
+    actual1 = sum_powers(3,1)
+    print('actual is: ', actual1)
+    #Test2
+    expected2 = 91
+    print('expected:', expected2)
+    actual2 = sum_powers(6, 2)
+    print('actual is: ', actual2)
+    # Test3
+    expected3 = 65
+    print('expected:', expected3)
+    actual3 = sum_powers(2, 6)
+    print('actual is: ', actual3)
 
 
 def sum_powers(n, p):
@@ -43,18 +58,24 @@ def sum_powers(n, p):
       -- sum_powers(100, 0.1) returns about 144.45655
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # -------------------------------------------------------------------------
+    answer = 0
+    for k in range (n):
+        answer = answer + (k+1)**p
+    return answer
+    print(answer)
+
 
 
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,6 +86,21 @@ def run_test_sum_powers_in_range():
     print('--------------------------------------------------')
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
+    # Test1
+    expected1 = 7
+    print('expected:', expected1)
+    actual1 = sum_powers_in_range(3, 4, 1)
+    print('actual is: ', actual1)
+    # Test2
+    expected2 = 91
+    print('expected:', expected2)
+    actual2 = sum_powers_in_range(1, 6, 2)
+    print('actual is: ', actual2)
+    # Test3
+    expected3 = 86
+    print('expected:', expected3)
+    actual3 = sum_powers_in_range(3,6, 2)
+    print('actual is: ', actual3)
 
 
 def sum_powers_in_range(m, n, p):
@@ -80,12 +116,19 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # -------------------------------------------------------------------------
+    answer = 0
+    for k in  range(m,n+1,1):
+        answer = answer + (k) ** p
+    return answer
+    print(answer)
+
+
 
 
 # -----------------------------------------------------------------------------
